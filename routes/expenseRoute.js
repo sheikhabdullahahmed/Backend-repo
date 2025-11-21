27a72338445f61e427c2d33e3b2ce9d732ee7a1c
sheikhabdullahahmed <sheikhabdullah456677@gmail.com>
2025-11-21 16:27:56 +0500

    done done

--- routes/expenseRoute.js
+++ routes/expenseRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 // import { addExpense, getAllExpenses, deleteExpense, downloadExpenseExcel } from '../controller/expenseController.js';
-import { addExpense } from '../controller/expenseController.js'; 
+import { addExpense, getAllExpenses, deleteExpense } from '../controller/expenseController.js'; 
 import requireAuth from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -8,6 +8,6 @@ const router = express.Router();
 router.post('/', requireAuth, addExpense);
 router.get('/', requireAuth, getAllExpenses);
 router.delete('/:id', requireAuth, deleteExpense);
-router.get('/download', requireAuth, downloadExpenseExcel);
+// router.get('/download', requireAuth, downloadExpenseExcel);
 
 export default router;
