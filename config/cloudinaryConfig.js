58e81a973a0b7b6f5cb064ce5fd642b8940b7c14
sheikhabdullahahmed <sheikhabdullah456677@gmail.com>
2025-11-21 12:55:53 +0500

    chnages

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,40 +1,23 @@
-// import multer from "multer";
-// import path from "path";
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => cb(null, "uploads/"),
-//   filename: (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname))
-// });
-
-// const upload = multer({ storage });
-
-// export default upload;
-
-
-
-
-
-
-
-dotenv.config();
 import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import dotenv from "dotenv";
 
+dotenv.config();
 
-
+// Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_SECRET,
 });
 
+// Configure Cloudinary storage for multer
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "uploads",
-    allowed_formats: ["jpg", "jpeg", "png"],
+    folder: "uploads", // Cloudinary folder name
+    allowed_formats: ["jpg", "jpeg", "png"], // only images
     public_id: (req, file) => `${Date.now()}-${file.originalname}`,
   },
 });
--- index.js
+++ index.js
@@ -8,17 +8,20 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutess from './controller/authController.js'
-import uploadRoute from './routes/authRoute.js'
+// import uploadRoute from './routes/authRoute.js'
+import serverless from "serverless-http";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
 const app = express();
+// export const handler = serverless(app);
 
 
 // Middleware
 app.use(cors({
+  origin: "*",
   origin: "http://localhost:5173", // ya jahan tumhara React run ho raha hai
   credentials: true
 }));
@@ -29,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //Routes
 app.use('/', authRoutess);
-app.use('/upload', uploadRoute);
+// app.use('/upload', uploadRoute);
 app.use('/income', incomeRoutes);
 app.use('/expense', expenseRoutes);
 
@@ -45,8 +48,10 @@ mongoose.connect(process.env.DATABASE_URL)
   .catch(err => console.log("MongoDB Connection Failed:", err));
 
 
-app.listen(process.env.PORT || 4000, () => {
-  console.log(`🟡 Server running on port ${process.env.PORT || 4000}`);
-});
+// app.listen(process.env.PORT || 4000, () => {
+//   console.log(`🟡 Server running on port ${process.env.PORT || 4000}`);
+// });
 
 
+
+export const handler = serverless(app);
